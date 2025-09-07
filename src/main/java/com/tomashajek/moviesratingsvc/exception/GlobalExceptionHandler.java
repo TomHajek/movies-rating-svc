0bd4066783b604a9ec8ab9c8e6cf2d576b24d7a9
_hayas <hajcus10@seznam.cz>
2025-09-07 10:08:19 +0200

    Updating exception handling

--- src/main/java/com/tomashajek/moviesratingsvc/exception/GlobalExceptionHandler.java
+++ src/main/java/com/tomashajek/moviesratingsvc/exception/GlobalExceptionHandler.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Objects;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
@@ -21,6 +22,29 @@ public class GlobalExceptionHandler {
             case INVALID_CREDENTIALS, UNAUTHORIZED -> status = HttpStatus.UNAUTHORIZED;
             case USER_NOT_FOUND -> status = HttpStatus.NOT_FOUND;
             default -> status = HttpStatus.BAD_REQUEST;
+        }
+        return ResponseEntity.status(status).body(Map.of("error", e.getMessage()));
+    }
+
+    @ExceptionHandler(MovieException.class)
+    public ResponseEntity<Map<String, String>> handleMovieException(MovieException e) {
+        HttpStatus status;
+        if (Objects.requireNonNull(e.getErrorType()) == MovieException.ErrorType.MOVIE_NOT_FOUND) {
+            status = HttpStatus.NOT_FOUND;
+        } else {
+            status = HttpStatus.BAD_REQUEST;
+        }
+        return ResponseEntity.status(status).body(Map.of("error", e.getMessage()));
+    }
+
+    @ExceptionHandler(RatingException.class)
+    public ResponseEntity<Map<String, String>> handleRatingException(RatingException e) {
+        HttpStatus status;
+        switch (e.getErrorType()) {
+            case RATING_ALREADY_EXISTS -> status = HttpStatus.CONFLICT;
+            case INVALID_VALUE -> status = HttpStatus.BAD_REQUEST;
+            case RATING_NOT_FOUND -> status = HttpStatus.NOT_FOUND;
+            default -> status = HttpStatus.INTERNAL_SERVER_ERROR;
         }
         return ResponseEntity.status(status).body(Map.of("error", e.getMessage()));
     }
